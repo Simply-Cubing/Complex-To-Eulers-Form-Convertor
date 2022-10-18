d2c53c88e5cdd8fc243377bdd8cb8eea5453435f
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2022-10-18 09:48:03 +0800

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -1,10 +1,12 @@
 import cmath
 print("(Enter Numbers In The Form a+bj")
 num = complex(input("Number -> "))
+#converts the numbers then rounds them
 costheta = cmath.acos(num.real)
 sintheta = cmath.acos(num.imag)
 rcostheta = round(costheta.real,3)
 rsintheta = round(sintheta.imag, 3)
+#prints the answer differently if one part is zero
 if rsintheta == 0:
   polar = f"""
   
