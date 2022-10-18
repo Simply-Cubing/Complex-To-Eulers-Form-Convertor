77ed9ce5176faea29cda98c3351a483266e6fcce
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2022-10-18 09:41:41 +0800

    Update convertor.py
    fixed bugs with the actual conversion process and rounds integers so your screen doesn’t look like a mess :)

--- convertor.py
+++ convertor.py
@@ -1,12 +1,24 @@
 import cmath
+print("(Enter Numbers In The Form a+bj")
 num = complex(input("Number -> "))
-costheta = cmath.acos(num)
-sintheta = cmath.acos(num)
-polar = f"""
+costheta = cmath.acos(num.real)
+sintheta = cmath.acos(num.imag)
+rcostheta = round(costheta.real,3)
+rsintheta = round(sintheta.imag, 3)
+if rsintheta == 0:
+  polar = f"""
+  
+e^({rcostheta}i)"""
+elif rcostheta == 0:
+  polar = f"""
+  
+e^({rsintheta}i)"""
+else:
+  polar = f"""
 
-e^(j{costheta}+{sintheta})"""
+e^(i({rcostheta}+{rsintheta})"""
 
 
 print(f"""
 
-Polar -> {polar}""")
+Polar Form of {num} = {polar}""")
